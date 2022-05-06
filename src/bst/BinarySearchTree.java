a240a919c8225ba949e6ede54a17fffc1ea3daf3
Tyler <teelar101@gmail.com>
2022-05-06 16:20:10 -0700

    post-order

--- src/bst/BinarySearchTree.java
+++ src/bst/BinarySearchTree.java
@@ -153,10 +153,18 @@ public class BinarySearchTree<T extends Comparable<T>> {
 	//Recurse on the children and then print the value in the current node
 	public void postOrder() {
 		System.out.println("Post Order test commit");
+		postOrderRecurse(root);
 	}
 	
-	public void postOrderRecurse(BSTNode<T> node) {
-		
+	public void postOrderRecurse(BSTNode<T> node)
+	{
+		if (node == null)
+		{
+			return;
+		}
+		postOrderRecurse(node.leftChild);
+		postOrderRecurse(node.rightChild);
+		System.out.print(node.data + " ");
 	}
 	
 	//Traverse the tree in an postorder fashion uses Stacks. 
